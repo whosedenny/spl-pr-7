d4a39bacb6b7bf5a3b31e50b8f935a201250650f
whosedenny <90749476+whosedenny@users.noreply.github.com>
2024-12-01 14:20:40 +0200

    add enums

---
+++ TSScripts/main.ts
@@ -0,0 +1,31 @@
+enum StudentStatus {
+    Active = "Active",
+    AcademicLeave = "Academic_Leave",
+    Graduated = "Graduated",
+    Expelled = "Expelled",
+}
+
+enum CourseType {
+    Mandatory = "Mandatory",
+    Optional = "Optional",
+    Special = "Special",
+}
+
+enum Semester {
+    First = "First",
+    Second = "Second",
+}
+
+enum Grade {
+    Excellent = 5,
+    Good = 4,
+    Satisfactory = 3,
+    Unsatisfactory = 2,
+}
+
+enum Faculty {
+    ComputerScience = "Computer_Science",
+    Economics = "Economics",
+    Law = "Law",
+    Engineering = "Engineering",
+}
