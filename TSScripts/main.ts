e42411d22121a7c32a450dcad38ed0d0e3469805
whosedenny <90749476+whosedenny@users.noreply.github.com>
2024-12-01 15:29:16 +0200

    Add comments

--- TSScripts/main.ts
+++ TSScripts/main.ts
@@ -1,35 +1,36 @@
+//Енам для статуса студенту
 enum StudentStatus {
     Active = "Active",
     AcademicLeave = "Academic_Leave",
     Graduated = "Graduated",
     Expelled = "Expelled",
 }
-
+//Енам для стипу курса
 enum CourseType {
     Mandatory = "Mandatory",
     Optional = "Optional",
     Special = "Special",
 }
-
+//Енам для номеру семестра
 enum Semester {
     First = "First",
     Second = "Second",
 }
-
+//Енам для оцінок
 enum Grade {
     Excellent = 5,
     Good = 4,
     Satisfactory = 3,
     Unsatisfactory = 2,
 }
-
+//Енам для виду факультету
 enum Faculty {
     ComputerScience = "Computer_Science",
     Economics = "Economics",
     Law = "Law",
     Engineering = "Engineering",
 }
-
+//Інтерфейс для студента
 interface Student {
     id: number;
     fullName: string;
@@ -39,7 +40,7 @@ interface Student {
     enrollmentDate: Date;
     groupNumber: string;
 }
-
+//Інтерфейс для курсу
 interface Course {
     id: number;
     name: string;
@@ -49,7 +50,7 @@ interface Course {
     faculty: Faculty;
     maxStudents: number;
 }
-
+//Інтерфейс для оцінки. Назва відрізняеться від вказаної вами через конфлікт назв між enum Grade та цим інтерфейсом
 interface IGrade {
     studentId: number;
     courseId: number;
@@ -57,19 +58,21 @@ interface IGrade {
     date: Date;
     semestr: Semester;
 }
-
+//Клас керування університетом
 class UniversityManagementSystem {
     private students: Student[] = [];
     private courses: Course[] = [];
     private grades: IGrade[] = [];
     private currentId: number = 1;
 
+    //Метод для додавання нових студентів
     enrollStudent(student: Omit<Student, "id">): Student {
         const newStudent = { ...student, id: this.currentId++ };
         this.students.push(newStudent);
         return newStudent;
     }
 
+    //Метод для реєстрування студета на курс
     registerForCourse(studentId: number, courseId: number): void {
         const student = this.students.find((s) => s.id === studentId);
         const course = this.courses.find((c) => c.id === courseId);
@@ -94,7 +97,7 @@ class UniversityManagementSystem {
             throw new Error("На курсі немає вільних місць");
         }
     }
-
+    //Метод для виставлення оцінки студенту
     setGrade(studentId: number, courseId: number, grade: Grade): void {
         const student = this.students.find((s) => s.id === studentId);
         const course = this.courses.find((c) => c.id === courseId);
@@ -122,7 +125,7 @@ class UniversityManagementSystem {
             semestr: course.semestr,
         });
     }
-
+    //Метод для виставлення оцінки студенту
     updateStudentStatus(studentId: number, newStatus: StudentStatus): void {
         const student = this.students.find((s) => s.id === studentId);
         if (!student) {
@@ -138,11 +141,11 @@ class UniversityManagementSystem {
 
         student.status = newStatus;
     }
-
+    //Метод для отримання студентів у факультеті
     getStudentsByFaculty(faculty: Faculty): Student[] {
         return this.students.filter((s) => s.faculty === faculty);
     }
-
+    //Метод для отримання усіх оцінок студента
     getStudentGrades(studentId: number): Grade[] {
         const student = this.students.find((s) => s.id === studentId);
         if (!student) {
@@ -152,13 +155,13 @@ class UniversityManagementSystem {
             .filter((g) => g.studentId === student.id)
             .map((g) => g.grade);
     }
-
+    //Метод для отримання курсів на факультеті у вказаному симестрі
     getAvailableCourses(faculty: Faculty, semester: Semester): Course[] {
         return this.courses.filter(
             (c) => c.faculty === faculty && c.semestr === semester
         );
     }
-
+    //Метод для отримання середнього балу студента
     calculateAverageGrade(studentId: number): number {
         const student = this.students.find((s) => s.id === studentId);
         if (!student) {
@@ -172,7 +175,7 @@ class UniversityManagementSystem {
         const total = studentGrades.reduce((sum, grade) => sum + grade, 0);
         return total / studentGrades.length;
     }
-
+    //Метод для отримання відмінників 
     getHonorStudentsByFaculty(faculty: Faculty): Student[] {
         return this.students.filter((student) => {
             const avgGrade = this.calculateAverageGrade(student.id);
